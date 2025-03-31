012adf806b09001dd2af1bb669158cb64f847107
Daniel Nęcka <119523330+JaToExe@users.noreply.github.com>
2025-03-31 16:39:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ function sendMessage(theme) {
 
 async function generateResponse(input, theme) {
     try {
-        const response = await fetch('http://127.0.0.1:4000/chat', {
+        const response = await fetch('https://chatbot-dq1v.onrender.com/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: input, theme: theme })
